3770c0cec9248d5a279986ddde4faf14c81b0abd
carloscarrion <carrion@edu.xunta.es>
2015-10-12 12:37:13 +0200

    Initial commit

---
+++ app/src/main/java/com/example/carlos/u3a_teacher/U3A_Teacher.java
@@ -0,0 +1,13 @@
+package com.example.carlos.u3a_teacher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class U3A_Teacher extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_u3_a__teacher);
+    }
+}
